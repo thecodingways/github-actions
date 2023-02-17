7570a75c686c43accc2dfbebead4e3c46db01a32
Mike Dao <mike@futurefuel.io>
2023-02-17 12:57:00 +1100

    Testing saving of screenshots and videos.

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    screenshotOnRunFailure: true,
+    screenshotsFolder: 'cypress/screenshots',
+    trashAssetsBeforeRuns: true,
+    videoCompression: 32,
+    videosFolder: 'cypress/videos',
+    video: true,
   },
 });
